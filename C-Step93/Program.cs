8d6289062e91a7980482c02f3c312fa129107516
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-07-25 21:17:52 -0500

    Add project files.

---
+++ C-Step93/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Step93
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my birthday! Month first:");
+            int BirthMonth = Convert.ToInt32(Console.ReadLine());
+            bool CorrectMonth = BirthMonth == 9;
+
+            while (!CorrectMonth)
+            {
+                switch (BirthMonth)
+                {
+                    case 8:
+                        Console.WriteLine("So close!");
+                        Console.WriteLine("Guess another month:");
+                        BirthMonth = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 10:
+                        Console.WriteLine("So close!");
+                        Console.WriteLine("Guess another month:");
+                        BirthMonth = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 9:
+                        CorrectMonth = true;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect!");
+                        Console.WriteLine("Guess another month:");
+                        BirthMonth = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+            Console.WriteLine("Correct! Now guess the day:");
+            int birthDay = Convert.ToInt32(Console.ReadLine());
+            bool CorrectDay = birthDay == 15;
+
+            do
+            {
+                switch (birthDay)
+                {
+                    case 14:
+                        Console.WriteLine("Close! Try again:");
+                        birthDay = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 16:
+                        Console.WriteLine("Close! Try again:");
+                        birthDay = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 15:
+                        Console.WriteLine("You win!");
+                        CorrectDay = true;
+                        break;
+                    default:
+                        Console.WriteLine("Try again:");
+                        birthDay = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            } while (!CorrectDay);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C-Step93/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("C-Step93")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("C-Step93")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2799b5b0-58b2-4bdb-a0c7-8f24eb3d79da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
